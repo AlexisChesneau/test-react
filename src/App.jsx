b5cc9ef6abba3f1aad9747a496da42344a84bf9a
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-03-29 16:00:56 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyTitle from './components/MyTitle'
+import MyCardPokemon from './components/PokemonCard'
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   return (
-    <div>
-      <MyTitle />
+    <div> {
+      pokemonList.map(nimporte => (
+      <MyCardPokemon pokemon={nimporte}/>
+      ))}
     </div>
   );
 }
-
 
 export default App
--- src/components/PokeMonCard.jsx
+++ src/components/PokeMonCard.jsx
@@ -1,8 +1,11 @@
-<figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="bulbasaur"/>
-    <figcaption>bulbasaur</figcaption>
-</figure>
+function MyCardPokemon({pokemon}) {
+  console.log(pokemon);
+  return (
+    <figure>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
 
-
-export default
+export default MyCardPokemon;
