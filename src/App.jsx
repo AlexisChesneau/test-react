77ebe2a45ac1160302e971f1297b5211ec7cae42
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-03-30 09:05:08 +0200

    react components2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyTitle from './components/MyTitle'
+import MyCardPokemon from "./components/PokeMonCard.jsx";
 
 function App() {
   return (
     <div>
-      <MyTitle />
+      <MyCardPokemon />
     </div>
   );
 }
--- src/components/PokeMonCard.jsx
+++ src/components/PokeMonCard.jsx
@@ -1,8 +1,23 @@
-<figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="bulbasaur"/>
-    <figcaption>bulbasaur</figcaption>
-</figure>
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+  
+  function MyCardPokemon () {
+     const pokemon = pokemonList[0];
+      return (
+          <figure>
+          {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+      )
+    }
 
-
-export default
+    export default MyCardPokemon
